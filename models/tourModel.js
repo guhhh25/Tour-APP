91ca6e67924c9b9109c8cd58f306146f76f11082
guhhh25 <guhhh25@gmail.com>
2022-11-03 15:03:31 -0300

    fix:env

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,8 +5,46 @@ const tourSchema = new mongoose.Schema({
       type: String,
       required: [true, 'a tour must have a name'] 
     },
-    rating: Number,
-    price: Number
+    duration:{
+      type:Number,
+      required:[true, 'a tour must have a duration'] 
+    },
+    maxGroupSize:{
+      type:Number,
+      required:[true, 'a tour must have a group size'] 
+    },
+    Difficulty:{
+      type:String,
+      required:[true, 'a tour must have a difficulty'] 
+    },
+    ratingsAverage:{
+      type:Number,
+      default: 4.5
+    },
+    ratinsQuantity:{
+      type:Number,
+      default:0
+    },
+    price:{
+      type:Number,
+      required:[true, 'a tour must have a price']
+    },
+    priceDiscount:{
+      type:Number
+    },
+    summery:{
+      type:String,
+      trim:true,
+      required:[true, 'a tour must have a description']
+    },
+    description:{
+      type: String,
+      trim:true
+    },
+    imageCover: {
+      type:String,
+      required:[true, 'a tour must have a  cover image']
+    }
   })
   
   const Tour = mongoose.model('Tour', tourSchema)
